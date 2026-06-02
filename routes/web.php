88b1ba67211693ce130ab85ac852f8265973913b
willfredcampos9-design <willfredcampos9@gmail.com>
2026-06-01 20:30:40 -0500

    Creación del index

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoriaController;
+
+/* Route::get('/', function () {
+    return view('welcome');
+}); */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+Route::resource('categorias', CategoriaController::class);
